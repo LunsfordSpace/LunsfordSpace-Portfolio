e01dd1a51b591839c00a1561415b5972f47e02e1
Shawn Lunsford <LunsfordSpace@gmail.com>
2021-07-09 10:19:52 -0700

    add preloader animation and background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("preloader-spin").style.display = "none";
+    document.getElementById("preloader-text").style.display = "none";
 });
